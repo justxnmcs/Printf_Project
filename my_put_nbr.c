36d22f28485a5d3bdf896a1c931293e9d7414d49
Justin <justin.marcos@myself.com>
2018-11-15 19:03:48 +0100

    putnbr

---
+++ my_put_nbr.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2018
+** my_put_nbr.c
+** File description:
+** libmy.a
+*/
+
+int my_put_nbr(int nb)
+{
+    int counter = 0;
+    if (nb < 0) {
+        nb *= -1;
+        my_putchar('-');
+    }
+    if (nb < 10) {
+        my_putchar(nb + '0');
+        return (0);
+    }
+    my_put_nbr(nb / 10);
+    my_putchar((nb % 10) + '0');
+    return (0);
+}
