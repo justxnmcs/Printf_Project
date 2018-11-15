ed9501dae0c39de476180bbb6a48bff7d8b9d972
Justin <justin.marcos@myself.com>
2018-11-15 19:04:21 +0100

    putstr

---
+++ my_putstr.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2018
+** my_putstr.c
+** File description:
+** Printf
+*/
+
+void my_put_str(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0') {
+        my_putchar(str[i]);
+        i += 1;
+    }
+}
